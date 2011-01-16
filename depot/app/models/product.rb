0852c5190088ca1c95b199815aacd558d7017251
f4nb0y <github@deathbeforedecaf.net>
2011-01-16 14:05:17 +1030

    vi app/models/product.rb

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+  validates_numericality_of	:price
+  validates_presence_of		:title, :descr, :image
+  validates_uniqueness_of	:title
+  validate			:sensible_price
+
+protected
+
+  def sensible_price
+    errors.add(:price, 'must be greater than zero') unless ! price.nil? && price > 0.0
+  end
+
 end
